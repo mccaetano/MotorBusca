89278106d00c81d1ceeb3a8f075ff0032d2de34d
marcelo <marcelo@marcelo-PC>
2014-07-18 05:02:50 -0300

    dev

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -23,9 +23,9 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('templates/header');
+		#$this->load->view('templates/header');
     	$this->load->view('home');
-		$this->load->view('templates/footer');
+		#$this->load->view('templates/footer');
 	}
 }
 
--- application/views/home.php
+++ application/views/home.php
@@ -1,94 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Motor de Busca</title>
+<meta name="description"
+	content="Motor de busca de imóveis, carros, motos, caminhões, empregos, produtos e temporadas.">
+<meta name="keywords"
+	content="imóvel, carro, moto, caminhão, emprego, produtos, temporada, veicúlo, casa, aapartamento, terreno">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Bootstrap -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet"
+	media="screen">
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
 
-<a href="#" class="thumbnail">
-	<img data-src="holder.js/200x200" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
-</a>
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144"
+	href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114"
+	href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72"
+	href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed"
+	href="assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/ico/favicon.png">
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<button type="button" class="btn btn-navbar" data-toggle="collapse"
+					data-target=".nav-collapse">
+					<span class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="brand" href="#">Motor de Busca</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#about">Imóveis</a></li>
+						<li><a href="#contact">Carros</a></li>
+						<li><a href="#contact">Empregos</a></li>
+						<li><a href="#contact">Produtos</a></li>
+						<li><a href="#contact">Temporadas</a></li>
+					</ul>
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+	<div style="height: 70px"></div>
+	<div class="container">
+		<div class="row">
+			<div class="text-center">
+				<img alt="Motor de Busca" src="assets/img/LogoEmpresa.png">
+			</div>
+		</div>
+		<div style="height: 30px"></div>
+		<div class="row">
+			<div class="text-center">O motor de busca de anúncios classificados
+				de imóveis, carros, empregos, produtos e imóveis para temporada</div>
+		</div>
+		<div style="height: 30px"></div
+		
+		
+		<div class="hero-unit text-center">
+			<div class="row">
+				<div class="span2">
+					<a href="#"><img alt="Imóveis" src="assets/img/Imoveis.jpg" width="48px" height="48px"><br>Imóveis</a>
+				</div>
+				<div class="span2">
+					<a href="#"><img alt="Carros" src="assets/img/Carros.jpg" width="48px" height="48px"><br>Carros</a>
+				</div>
+				<div class="span2">
+					<a href="#"><img alt="Empregos" src="assets/img/Empregos.jpg" width="48px" height="48px"><br>Empregos</a>
+				</div>
+				<div class="span2">
+					<a href="#"><img alt="Produtos" src="assets/img/Produtos.jpg" width="48px" height="48px"><br>Produtos</a>
+				</div>
+				<div class="span2">
+					<a href="#"><img alt="Temporadas" src="assets/img/Temporadas.jpg" width="48px" height="48px"><br>Temporadas</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
 
-<div class="list-group">
-	<a href="#" class="list-group-item">
-		<div class="row">
-			<div class="col-md-6"><h1>ImÃ³veis<h1></div>
-			<div class="col-md-6">
-				<form role="form" class="form-inline" action="<?php echo base_url() ?>pesquisa" method="post">
-					<fieldset>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Preencha o texto aqui">
-							<span class="input-group-btn">
-        						<button class="btn btn-default" type="submit" >Buscar</button>
-      						</span>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>		
-	</a>
-	<a href="#" class="list-group-item">
-		<div class="row">
-			<div class="col-md-6"><h1>Carros<h1></div>
-			<div class="col-md-6">
-				<form role="form" class="form-inline">
-					<fieldset>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Preencha o texto aqui">
-							<span class="input-group-btn">
-        						<button class="btn btn-default" type="button">Buscar</button>
-      						</span>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-	</a>
-	<a href="#" class="list-group-item">
-		<div class="row">
-			<div class="col-md-6"><h1>Motos<h1></div>
-			<div class="col-md-6">
-				<form role="form" class="form-inline">
-					<fieldset>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Preencha o texto aqui">
-							<span class="input-group-btn">
-        						<button class="btn btn-default" type="button">Buscar</button>
-      						</span>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-	</a>
-	<a href="#" class="list-group-item">
-		<div class="row">
-			<div class="col-md-6"><h1>CaminhÃµes<h1></div>
-			<div class="col-md-6">
-				<form role="form" class="form-inline">
-					<fieldset>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Preencha o texto aqui">
-							<span class="input-group-btn">
-        						<button class="btn btn-default" type="button">Buscar</button>
-      						</span>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-	</a>
-	
-	<a href="#" class="list-group-item">
-		<div class="row">
-			<div class="col-md-6"><h1>Nautica<h1></div>
-			<div class="col-md-6">
-				<form role="form" class="form-inline">
-					<fieldset>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Preencha o texto aqui">
-							<span class="input-group-btn">
-        						<button class="btn btn-default" type="button">Buscar</button>
-      						</span>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-	</a>
-</div>
-
+</body>
+</html>
