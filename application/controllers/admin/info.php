f14fa9a0be7f814267f1bc263783d809427dbf11
marcelo <marcelo@marcelo-PC>
2014-08-26 00:19:22 -0300

    dev

--- application/controllers/admin/info.php
+++ application/controllers/admin/info.php
@@ -1,7 +1,7 @@
 <?php 
 if ( ! defined('BASEPATH')) { exit('No direct script access allowed');}
 
-class home extends CI_Controller {
+class Info extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->helper(array('form'));
