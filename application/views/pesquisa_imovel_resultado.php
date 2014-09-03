17ec99913e8d62c1cd9b8f15a4301362166b1d42
Administrator <Administrator@172.31.22.48>
2014-09-03 01:40:41 -0300

    dev

--- application/views/pesquisa_imovel_resultado.php
+++ application/views/pesquisa_imovel_resultado.php
@@ -2,10 +2,10 @@
 <hr>
 <a href="<?php echo $row->ac_url; ?>">
 <div class="row-fluid">
-	<div class="span2">
+	<div class="span4">
 		<img alt="<?php echo $row->acf_titulo; ?>" src="<?php echo $row->acf_url; ?>" width="150" height="150">
 	</div>
-	<div class="span10">
+	<div class="span8">
 		<div class="row">
 			<div class="span8"><?php echo $row->ac_title; ?></div>
 			<div class="span2"><?php echo $row->ac_preco; ?></div>
