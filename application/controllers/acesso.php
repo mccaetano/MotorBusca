f586a10f058abb87fa640f426e5fff5d630317f6
Administrator <Administrator@172.31.22.48>
2014-09-03 07:57:53 -0300

    dev

--- application/controllers/acesso.php
+++ application/controllers/acesso.php
@@ -4,12 +4,12 @@ if ( ! defined('BASEPATH')) { exit('No direct script access allowed');}
 class Acesso extends CI_Controller {
     function __construct() {
         parent::__construct();
-        $this->load->helper(array('form'));
+        $this->load->helper(array('form', 'url'));
     }
     
 	public function login($pagereturn = FALSE)
 	{
-		if (!$pagereturn) { $pagereturn = "/"; } else { $pagereturn = base64_decode($pagereturn); } 
+		if (!$pagereturn) { $pagereturn = "home"; } else { $pagereturn = base64_decode($pagereturn); } 
 		
 		$this->load->library('auth');
 		
@@ -51,5 +51,44 @@ class Acesso extends CI_Controller {
 		$this->load->library('auth');
 		$this->auth->logout();
 		redirect(base_url());
+	}
+	
+	function cadastro($pagereturn = FALSE) {
+		$method =  (string)$_SERVER["REQUEST_METHOD"];
+		if (!$pagereturn) { $pagereturn = "home"; } else { $pagereturn = base64_decode($pagereturn); }
+		 
+		$this->load->model ( "mbperfil", "perfil" );
+		 
+		if ($method == "POST") {
+			$this->load->library('form_validation');
+		
+			$this->form_validation->set_rules('iEmail', 'Email é obtigatório', 'required');
+			$this->form_validation->set_rules('iSenha', 'Senha', 'trim|required|xss_clean|max_length[10]|matches[icSenha]');
+		
+			if ($this->form_validation->run() == true) {
+				$row = array(
+						'nome_completo' => $this->input->post('iNome'),
+						'email' => $this->input->post('iEmail'),
+						'data_nascimento' => $this->input->post('iDataNascimento'),
+						'senha' => base64_encode($this->input->post('iSenha')),
+						'ativo' => '1',
+						't_mb_perfil_acesso_pra_id' => '2'
+				);
+				 
+				$perfil_id = $this->perfil->Adicionar ($row);
+
+				$this->load->library('auth');
+				$this->auth->login($perfil_id, TRUE);
+				
+				redirect($pagereturn);
+			}
+		}
+		 
+		$data = array(
+				'pagereturn' => base64_encode($pagereturn)
+		);
+		$this->load->view('templates/header', $data);
+		$this->load->view('login', $data);
+		$this->load->view('templates/footer', $data);
 	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,48 +1,94 @@
 <div class="row-fluid">
-	<div class="span6 offset3"><h1>Acesso do Usuário</h1></div>
+	<div class="span12"><h1>Acesso do Usuário</h1></div>
 </div>
 <div class="clear"></div>
-<div class="row-fluid">
-<div class="span6 offset3">
-<?php
+
+<?php 
 if (isset ( $error ) && $error != "") {
-	
- echo "<div class=\"alert alert-error\">";
- echo $error;
-	echo "</div>";
+echo "<div class=\"row-fluid\">";
+echo "	<div class=\"span6\">";
+echo "		<div class=\"alert alert-error\">";
+echo $error;
+echo "		</div>";
+echo "	</div>";
+echo "</div>";
 } ?>
-		<div class="well">
-		
+
 <div class="row-fluid">
-<?php echo form_open('acesso/login/' . $pagereturn, array('class' => 'form-horizontal')); ?>
-<fieldset>
-<div class="control-group">
-	<label class="control-label" for="username">Email: </label>
-	<div class="controls">
-		<input type="text" name="username" id="username" />
+<div class="span6">
+	<div class="well">
+		<h3>Login</h3>
+		<form action="<?php echo base_url();?>acesso/login/<?php echo $pagereturn; ?>" method="post" class="form-horizontal">
+			<fieldset>
+				<div class="control-group">
+					<label class="control-label" for="username">Email: </label>
+					<div class="controls">
+						<input type="text" name="username" id="username" />
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="password">Senha: </label>
+					<div class="controls">
+						<input type="password" name="password" id="password" />
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="controls">
+						<input type="checkbox"
+								name="remember" value="1" id="remember" /> Lembrar me.
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="controls">
+						<button class="btn btn-primary" type="submit">Login</button>
+					</div>
+				</div>
+			</fieldset>
+		</form>
 	</div>
 </div>
-<div class="control-group">
-	<label class="control-label" for="password">Senha: </label>
-	<div class="controls">
-		<input type="password" name="password" id="password" />
+<div class="span6">
+	<div class="well">
+		<h3>Cadastrar-se</h3>
+		<form action="<?php echo base_url();?>acesso/cadastro/<?php echo $pagereturn; ?>" method="post" class="form-horizontal">
+			<fieldset>
+				<div class="control-group">
+				    <label class="control-label" for="iNome">Nome:</label>
+				    <div class="controls">
+				      <input type="text" id="iNome" name="iNome" placeholder="Nome completo" value="<?php echo set_value("iNome"); ?>">
+				    </div>
+			  	</div>
+			  	<div class="control-group">
+				    <label class="control-label" for="iEmail">Email:</label>
+				    <div class="controls">
+				      <input type="email" id="iEmail" name="iEmail" placeholder="Email" value="<?php echo set_value("iEmail"); ?>">
+				    </div>
+			  	</div>
+			  	<div class="control-group">
+				    <label class="control-label" for="iDataNascimento">Data Nascimento:</label>
+				    <div class="controls">
+				      <input type="date" id="iDataNascimento" name="iDataNascimento" placeholder="Data de Nascimento" value="<?php echo set_value("iDataNascimento"); ?>">
+				    </div>
+			  	</div>
+			  	<div class="control-group">
+				    <label class="control-label" for="iSenha">Senha:</label>
+				    <div class="controls">
+				      <input type="password" id="iSenha" name="iSenha" placeholder="Preencha a Senha">
+				    </div>
+			  	</div>
+			  	<div class="control-group">
+				    <label class="control-label" for="icSenha">Confirma a Senha:</label>
+				    <div class="controls">
+				      <input type="password" id="icSenha" name="icSenha" placeholder="Confime a Senha">
+				    </div>
+			  	</div>		  	
+			  	<div class="control-group">
+				    <div class="controls">
+				    	<button class="btn btn-primary" type="submit">Salvar</button>
+				    </div>
+			  	</div>			
+			</fieldset>
+		</form>
 	</div>
-</div>
-<div class="control-group">
-	<div class="controls">
-		<input type="checkbox"
-				name="remember" value="1" id="remember" /> Lembrar me.
-	</div>
-</div>
-<div class="control-group">
-	<div class="controls">
-		<input type="submit"
-				value="login" />
-	</div>
-</div>
-</fieldset>
-			</form>
-</div>
-</div>
 </div>
 </div>
