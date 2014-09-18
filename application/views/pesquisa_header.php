0656d6b8c751338641e92a6fc2c9f08f095cfc40
marcelo <marcelo@marcelo-pc>
2014-09-18 00:26:52 -0300

    dev

---
+++ application/views/pesquisa_header.php
@@ -0,0 +1,10 @@
+<form action="<?php echo base_url();?>pesquisa/<?php echo $find; ?>" method="post">
+<div class="masthead">
+	<div class="pull-center">
+	<a href="<?php echo base_url(); ?>">
+		<img align="middle" alt="Logo da empresa"
+			src="<?php echo base_url();?>assets/img/LogoEmpresa.png">
+		</a>
+	</div>
+	<hr>
+</div>
